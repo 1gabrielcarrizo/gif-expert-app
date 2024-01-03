f94dd263ae8ccd81ae7ee0d876795afcbd7f7d10
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-02 23:38:56 -0300

    category list created

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react"
 
 
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(["Dragon Ball", "Yuri On Ice"])
+    console.log(categories)
+
     return (
         <>
         {/* titulo */}
-        
+
             <h1>GifExpertApp</h1>
             {/* input */}
 
             {/* listado de gif */}
+            <ol>
+                {categories.map((category) => (
+                    <li key={category}>{category}</li>
+                ))}
+            </ol>
                 {/* gif item */}
         </>
     )
