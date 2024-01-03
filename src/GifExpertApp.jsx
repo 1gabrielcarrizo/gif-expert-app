396e62eb5312ea7c15696dc5b9c0804a2948f6ec
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-02 23:22:29 -0300

    creating GifExpertApp + adding styles

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+
+
+const GifExpertApp = () => {
+    return (
+        <>
+        {/* titulo */}
+        
+            <h1>GifExpertApp</h1>
+            {/* input */}
+
+            {/* listado de gif */}
+                {/* gif item */}
+        </>
+    )
+}
+
+export default GifExpertApp
