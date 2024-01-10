cb23dbe2d4d6e85bd6b8d3a4ab01f88e1f96f216
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-10 19:41:23 -0300

    new GifGrid component created

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import AddCategory from "./components/AddCategory"
+import GifGrid from "./components/GifGrid"
 
 
 const GifExpertApp = () => {
@@ -7,29 +8,28 @@ const GifExpertApp = () => {
     const [categories, setCategories] = useState(["Dragon Ball", "Yuri On Ice"])
 
     const onAddCategory = (newCategory) => {
-        if(categories.includes(newCategory)) return; // si ya existe en la lista, no lo agregara de nuevo
+        if (categories.includes(newCategory)) return; // si ya existe en la lista, no lo agregara de nuevo
         setCategories([...categories, newCategory])
     }
 
     return (
         <>
-            {/* titulo */}
-
             <h1>GifExpertApp</h1>
-            {/* input */}
+
             <AddCategory
                 onNewCategory={onAddCategory}
-            // setCategories={setCategories}
             />
 
-            {/* listado de gif */}
 
-            <ol>
-                {categories.map((category) => (
-                    <li key={category}>{category}</li>
-                ))}
-            </ol>
-            {/* gif item */}
+            {
+                categories.map((category) => (
+                    <GifGrid
+                        key={category}
+                        cateogry={category}
+                    />
+                ))
+            }
+
         </>
     )
 }
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,11 @@
+
+
+const GifGrid = ({ cateogry }) => {
+    return (
+        <>
+            <h3>{cateogry}</h3>
+        </>
+    )
+}
+
+export default GifGrid
