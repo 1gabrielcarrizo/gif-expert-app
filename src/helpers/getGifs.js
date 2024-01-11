0092ac78d1d0365231a14d3ccfea6cdebad89d57
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-10 21:58:35 -0300

    useEffect added

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /*
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=x9gxiOW8sqFk7nmOsgiBPp909W9sTRm5&q=${ category }&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=x9gxiOW8sqFk7nmOsgiBPp909W9sTRm5&q=${ category }&limit=10`
     const resp = await fetch( url );
     const {data} = await resp.json(); // "data" es un arreglo
     const gifs = data.map((img) => ({ // retornamos un objeto
@@ -16,7 +16,7 @@ export const getGifs = async (category) => {
 }
 */
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=x9gxiOW8sqFk7nmOsgiBPp909W9sTRm5&q=${ category }&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=x9gxiOW8sqFk7nmOsgiBPp909W9sTRm5&q=${ category }&limit=10`
     const resp = await axios(url)
     const {data} = await resp.data; // "data" es un arreglo
     const gifs = data.map((img) => ({ // retornamos un objeto
