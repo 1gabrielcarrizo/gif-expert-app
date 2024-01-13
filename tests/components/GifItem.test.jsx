b236edba5fce28805cca2960bae8f1bc7665f56c
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-12 21:32:28 -0300

    test in GifItem done

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components"
 
 describe('Pruebas en el componente <GifItem/>', () => {
@@ -9,8 +9,29 @@ describe('Pruebas en el componente <GifItem/>', () => {
 
     test('debe de hacer match con el snapshot', () => {
         // 2 - para hacer la evaluacion del snapshot se debe usar el container
-        const { container } = render(<GifItem title={ title } url={ url } />)
+        const { container } = render(<GifItem title={title} url={url} />)
         // 3 - observacion
         expect(container).toMatchSnapshot()
+    })
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+        // 2 - renderizamos nuestro componente
+        render(<GifItem title={title} url={url} />)
+        // screen.debug()
+        // 3 - observacion
+        // expect(screen.getByRole('img').src).toBe( url )
+        // expect(screen.getByRole('img').alt).toBe( title )
+        // podemos simplificar el codigo de la siguiente manera
+        const { src, alt } = screen.getByRole('img')
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    })
+
+    test('debe de mostrar el titulo en el componente', () => {
+        // 2 - renderizamos nuestro componente
+        render(<GifItem title={title} url={url} />)
+        // 3 - observacion
+        expect(screen.getByText(title)).toBeTruthy();
     })
 })
